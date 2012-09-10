c3756eba143c1d4037355390f3577d581b8e3266
James Carpenter <jamesc@nrl.com.au>
2012-09-10 20:55:49 +1000

    main.html added, menu bar to exit to main menu added

---
+++ js/appbar.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+    var page = WinJS.UI.Pages.define("main.html", {
+        ready: function (element, options) {
+            document.getElementById("cmdDelete")
+                .addEventListener("click", doClickDelete, false);
+        },
+    });
+
+    // Command button functions
+    function doClickDelete() {
+        window.open("default.html", "_self");
+    }
+})();
